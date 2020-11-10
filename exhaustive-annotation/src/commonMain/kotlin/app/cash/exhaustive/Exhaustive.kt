772c994c4394e7eda39ff1f334c701a217977579
Jake Wharton <jw@squareup.com>
2020-11-10 10:41:04 -0500

    Document the annotation

--- exhaustive-annotation/src/commonMain/kotlin/app/cash/exhaustive/Exhaustive.kt
+++ exhaustive-annotation/src/commonMain/kotlin/app/cash/exhaustive/Exhaustive.kt
@@ -2,5 +2,31 @@ package app.cash.exhaustive
 
 import kotlin.annotation.AnnotationRetention.SOURCE
 
+/**
+ * Denote a `when` statement which is required to exhaustively cover all cases.
+ *
+ * The Kotlin compiler will only enforce exhaustiveness for a `when` used as an expression. This
+ * annotation extends that same enforcement to statement `when`s. Additionally, these `when`s are
+ * forbidden from having an `else` branch since it is mutually exclusive with exhaustiveness.
+ *
+ * ```kotlin
+ * enum class RouletteColor { Red, Black, Green }
+ *
+ * fun printColor(color: RouletteColor) {
+ *   @Exhaustive
+ *   when (subject) {
+ *     Red -> println("red")
+ *     Black -> println("black")
+ *   }
+ * }
+ * ```
+ * Compilation of the above will fail with:
+ * ```
+ * e: Example.kt:5: @Exhaustive when is not exhaustive!
+ *
+ * Missing branches:
+ * - RouletteColor.Green
+ * ```
+ */
 @Retention(SOURCE)
 annotation class Exhaustive
